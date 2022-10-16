f43ea55de4c657fcaa389071f40ca6db2a292392
codingmatemoon <abc9529@naver.com>
2022-10-17 08:51:19 +0900

    기존 application/x-www-form-urlencoded content type post 테스트

--- src/main/java/com/springbootvuejs/controller/PostController.java
+++ src/main/java/com/springbootvuejs/controller/PostController.java
@@ -1,13 +1,20 @@
 package com.springbootvuejs.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // RestController를 활용하면 @Controller, @ResponseBody 등 Controller보다 좀 더 편하게 web 관련처리를 할 수 있습니다.
 @RestController
 public class PostController {
 
-    @GetMapping("/posts")
+    // Http Method
+    // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+    // 글 등록
+    // POST Method
+
+    //@GetMapping("/posts")
+    @PostMapping("/posts")
 //    @RequestMapping(method = RequestMethod.GET, path ="/v1/posts")
     public String get() {
         return "Hello World";
--- src/test/java/com/springbootvuejs/controller/PostControllerTest.java
+++ src/test/java/com/springbootvuejs/controller/PostControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -23,9 +25,12 @@ class PostControllerTest {
 
     @Test
     @DisplayName("/posts 요청시 Hello World를 출력한다.")
-    void indexTest() throws Exception {
+    void indexTest() throws Exception {  // 가능하면 application/json을 권장합니다. (기존 application/x-www-form-urlencoded)
         //expected
-        mockMvc.perform(get("/posts"))
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("title", "글 제목 테스트")
+                        .param("content", "글 내용 테스트"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World"))
                 .andDo(MockMvcResultHandlers.print());
