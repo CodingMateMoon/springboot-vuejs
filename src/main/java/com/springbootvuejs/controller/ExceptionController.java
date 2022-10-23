2aa118cd7f4f278a1689ba6e312ace20e0d8c4d6
codingmatemoon <abc9529@naver.com>
2022-10-24 07:59:26 +0900

    @ResponseBody 활용. 모델을 뷰로 렌더링하지 않고 반환된 객체를 응답 본문에 쓰도록 지시

--- src/main/java/com/springbootvuejs/controller/ExceptionController.java
+++ src/main/java/com/springbootvuejs/controller/ExceptionController.java
@@ -2,17 +2,29 @@ package com.springbootvuejs.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
 public class ExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(Exception.class)
-    public void exceptionHandler(Exception e) {
-        log.error("exceptionHandler error", e);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String message = fieldError.getDefaultMessage();
+        Map<String, String> response = new HashMap<>();
+        response.put(field, message);
+        return response;
     }
 }
